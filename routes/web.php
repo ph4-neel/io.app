2c0c48cab3bd79c26e1afb0d0054cf8fa2a0303f
aswinrajkarthikeyan <www.aswinraj6369@gmail.com>
2022-10-03 16:50:06 +0530

    aswin commit

--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,52 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/',[HomeController::class,'HomePage']);
 
+Route::get('/login',function()
+{
+    return view('layouts.login');
+});
 
+Route::get('/register',function()
+{
+    return view('layouts.register');
+});
+
+Route::get('/forgot',function()
+{
+    return view('layouts.forgotpass');
+});
+
+Route::get('/logout',function()
+{
+    return view('layouts.logout');
+});
+
+Route::get('/instituteregister',function()
+{
+    return view('layouts.instituteregister');
+});
+
+Route::get('/institutelogin',function()
+{
+    return view('layouts.institutelogin');
+});
+
+Route::get('/enterpriseregister',function()
+{
+    return view('layouts.enterpriseregister');
+});
+
+Route::get('/enterpriselogin',function()
+{
+    return view('layouts.enterpriselogin');
+});
+
+Route::get('/companyregister',function()
+{
+    return view('layouts.companyregister');
+});
+
+Route::get('/companylogin',function()
+{
+    return view('layouts.companylogin');
+});
